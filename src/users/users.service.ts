307d11eed20b2309e21c1e833a116620c5c4af8f
Jongok Yoon <bak01076@gmail.com>
2021-07-26 17:21:45 +0200

    createaccount resolver and service adapted

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,21 +10,22 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
 
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        //make error
-        return;
+        return { ok: false, error: 'There is a user with this email' };
       }
       await this.users.save(this.users.create({ email, password, role }));
-      return true;
+      return { ok: true };
     } catch (error) {
-      //make error
-      return;
+      return { ok: false, error: "Couldn't create account" };
     }
 
-    //check new user
     //create user & hash the password
   }
 }
